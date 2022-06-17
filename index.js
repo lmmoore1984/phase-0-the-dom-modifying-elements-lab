6dff1abbdc412b08c9af7cee2c0ff6b2ff79f429
Lori <michellemoore401@gmail.com>
2022-06-17 00:19:09 -0400

    commit tree 1da70f85dd8eed1b6863594b74aea5b9608bc45e

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+main.remove(main);
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Michelle is the champion"
+document.body.append("h1")
